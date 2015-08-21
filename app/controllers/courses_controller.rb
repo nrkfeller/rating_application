ecb294221f6db31890a3eddfcc30f8a530c7bd4b
Nicolas Feller <nrkfeller@gmail.com>
2015-08-21 15:13:22 +0000

    Review ratings to stars and added average

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,6 +8,11 @@ class CoursesController < ApplicationController
   def show
     @courses = Course.all
     @reviews = Review.where(course_id: @course.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
